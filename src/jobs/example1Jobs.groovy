e51569920b5cddfb782c3fc7e6e337d74906ffd1
Ramesh Pittala <ramesh.pittala@equinox.com>
2020-10-09 11:47:23 +0530

    changd the repo

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -1,5 +1,5 @@
 String basePath = 'example1'
-String repo = 'sheehan/gradle-example'
+String repo = 'pittalar/spring-boot-data-H2-embedded'
 
 folder(basePath) {
     description 'This example shows basic folder/job creation.'
