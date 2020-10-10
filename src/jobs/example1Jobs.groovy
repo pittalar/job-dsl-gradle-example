da08d4d90a20ad6c144d60e2c690ef278552f5d8
pittalar <34675997+pittalar@users.noreply.github.com>
2020-10-11 01:17:58 +0530

    Update example1Jobs.groovy
    dsl

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -1,10 +1,8 @@
-multibranchPipelineJob('example') {
+multibranchPipelineJob('Dsl') {
     branchSources {
         git {
             id('123456789') // IMPORTANT: use a constant and unique identifier
             remote('https://github.com/pittalar/simplespringboot.git')
-            credentialsId('github-ci')
-            includes('JENKINS-*')
         }
     }
     orphanedItemStrategy {
