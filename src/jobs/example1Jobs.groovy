970743982fd0839404736e74e21d950bf6d11e6e
pittalar <34675997+pittalar@users.noreply.github.com>
2020-10-11 01:39:58 +0530

    Update example1Jobs.groovy
    id added

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -1,6 +1,7 @@
 multibranchPipelineJob('Dsl') {
     branchSources {
         git {
+            id('123456789')
             remote('https://github.com/pittalar/simplespringboot.git')
         }
     }
