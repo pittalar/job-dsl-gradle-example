7a0c8b4b096c31320a2450ee5e110ff8687e0ea3
pittalar <34675997+pittalar@users.noreply.github.com>
2020-10-11 01:03:47 +0530

    Update example1Jobs.groovy
    multibranch pipeline

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -1,29 +1,15 @@
-pipelineJob('Your App Pipeline') { 
-
-  def repo = 'https://github.com/pittalar/job-dsl-gradle-example.git' 
- 
-  description("Your App Pipeline") 
-  keepDependencies(false) 
-
-  properties{ 
-
-    githubProjectUrl (repo) 
-    rebuild { 
-      autoRebuild(false) 
-    } 
-  } 
-
-  definition { 
-
-    cpsScm { 
-      scm { 
-        git { 
-          remote { url(repo) } 
-          branches('master') 
-          scriptPath('Jenkinsfile') 
-          extensions { }  // required as otherwise it may try to tag the repo, which you may not want 
-        } 
-      } 
-    } 
-  }
+multibranchPipelineJob('example') {
+    branchSources {
+        git {
+            id('123456789') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/jenkinsci/job-dsl-plugin.git')
+            credentialsId('github-ci')
+            includes('JENKINS-*')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
 }
